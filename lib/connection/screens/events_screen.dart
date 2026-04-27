f07aab50b6331c8b923c0c0d46eab9fc0d064a0a
Nabia Lavala <nabialavala@gmail.com>
2026-04-27 16:01:24 -0400

    Added Events Screen

--- lib/connection/screens/events_screen.dart
+++ lib/connection/screens/events_screen.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../services/event_service.dart';
 
 class EventsScreen extends StatelessWidget {
-  const EventsScreen({super.key});
+  final EventService _eventService = EventService();
+  EventsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Events')),
-      body: Center(child: Text('Events screen placeholder')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _eventService.getEvents(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No events found'));
+          }
+
+          final events = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: events.length,
+            itemBuilder: (context, index) {
+              final event = events[index];
+              final data = event.data() as Map<String, dynamic>;
+
+              return Card(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 6,
+                ),
+                child: ListTile(
+                  title: Text(data['title'] ?? 'No Title'),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(data['description'] ?? ''),
+                      const SizedBox(height: 4),
+                      Text(
+                        (data['date'] as Timestamp)
+                          .toDate()
+                          .toString(),
+                        style: const TextStyle(fontSize: 12),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
