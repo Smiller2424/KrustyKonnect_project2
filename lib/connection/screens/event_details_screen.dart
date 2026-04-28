fa6e60e5169da8681edbe2dad7e0818891d17b08
Nabia Lavala <nabialavala@gmail.com>
2026-04-28 14:15:11 -0400

    Added event details screen with RSVP feature implemented

---
+++ lib/connection/screens/event_details_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import '../services/event_service.dart';
+
+class EventDetailsScreen extends StatelessWidget {
+  final String eventId;
+  final String currentUserId;
+
+  const EventDetailsScreen({
+    super.key,
+    required this.eventId,
+    required this.currentUserId,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final EventService eventService = EventService();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Event Details")),
+      body: StreamBuilder(
+        stream: eventService.getEvents(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final docs = snapshot.data!.docs;
+          final event = docs.firstWhere((doc) => doc.id == eventId);
+          //check if user already rsvp'd
+          final attendees = event['attendees'] ?? [];
+          final isAttending = attendees.contains(currentUserId);
+
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  event['itle'],
+                  style: const TextStyle(fontSize: 20)
+                ),
+                Text("Location: ${event['location']}"),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if(isAttending) {
+                      eventService.cancelRsvp(
+                        eventId: eventId,
+                        userId: currentUserId,
+                      );
+                    } else {
+                      eventService.rsvpToEvent(
+                        eventId: eventId,
+                        userId: currentUserId,
+                      );
+                    }
+                  },
+                  //change whether or not they are coming
+                  child: Text(isAttending ? "Leave Event" : "Join Event"),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
